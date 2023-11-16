b3f0e7a80bdf64290c28e0903ee7158053ca1f82
Juancho10 <juanitolizarazu18@gmail.com>
2023-11-16 12:26:38 -0500

    ciclo de juego

---
+++ JavaScript/exercise_five/app.js
@@ -0,0 +1,11 @@
+
+function reducir(numInicial) {
+    let contador = 0;
+    while (numInicial>contador) {
+        console.log("hola")
+        contador = contador + numInicial;
+    }
+        
+}
+
+console.log(reducir(100));
