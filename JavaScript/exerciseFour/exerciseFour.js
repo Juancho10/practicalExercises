53bc4e2c9d8018c3b00d2e820abc5b20222aeb8a
Juancho10 <juanitolizarazu18@gmail.com>
2024-04-01 17:04:04 -0500

    Maquetación principal juego

---
+++ JavaScript/exerciseFour/exerciseFour.js
@@ -0,0 +1,14 @@
+const text = document.createElement("p")
+
+function poema (word){
+    const result = word[0].toUpperCase();
+    return "la palabra es" + result
+}
+
+console.log(poema("hola").length);
+const body = document.querySelector("div");
+console.log(body);
+
+
+//text.appendChild(poema("hola"));
+
