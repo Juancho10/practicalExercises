986bf56867f1984d07bb39995cc2955105c72546
Juancho10 <juanitolizarazu18@gmail.com>
2024-07-26 17:06:31 -0500

    binario

--- JavaScript/exercise_one/app.js
+++ JavaScript/exercise_one/app.js
@@ -1,8 +1,30 @@
-
+/**
+ * 
 let resultado = 0;
 
 while(resultado < 11){
     resultado++
     console.log(resultado);
 }
+ * 
+ */
 
+let numeroA = 31;
+let numeroB = 1;
+
+let resultado = numeroA+numeroB;
+
+let modulo = resultado / 2;
+//console.log(modulo)
+// convertir a binario
+//console.log(resultado);
+while(resultado > 1){
+    resultado = parseInt(resultado / 2);
+
+    console.log(resultado+ "\n");
+    modulo = resultado % 2;
+    //console.log(modulo);
+    //return modulo;
+}
+//console.log(modulo)
+ 
