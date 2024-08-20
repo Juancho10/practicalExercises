7c35367080a912bbb291fe187cc9631c0fab1590
Juancho10 <juanitolizarazu18@gmail.com>
2024-08-20 17:08:50 -0500

    add function sum

--- JavaScript/exercise_one/app.js
+++ JavaScript/exercise_one/app.js
@@ -1,30 +1,21 @@
-/**
- * 
-let resultado = 0;
-
-while(resultado < 11){
-    resultado++
-    console.log(resultado);
-}
- * 
- */
-
-let numeroA = 31;
+let numeroA = 1;
 let numeroB = 1;
 
 let resultado = numeroA+numeroB;
 
 let modulo = resultado / 2;
-//console.log(modulo)
-// convertir a binario
-//console.log(resultado);
-while(resultado > 1){
+console.log(modulo);
+
+function suma(numA,numB){
+    return numA+numB
+}
+
+/*while(resultado > 1){
     resultado = parseInt(resultado / 2);
 
     console.log(resultado+ "\n");
     modulo = resultado % 2;
-    //console.log(modulo);
-    //return modulo;
-}
-//console.log(modulo)
+    
+}*/
+
  
