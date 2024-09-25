b52cfe9c5908cbaf5776edc19fb551da986b838d
Juancho10 <juanitolizarazu18@gmail.com>
2024-09-25 17:18:30 -0500

    primeros pasos de archivo js

--- JavaScript/Generador de Contraseñas/main.js
+++ JavaScript/Generador de Contraseñas/main.js
@@ -1,4 +1,25 @@
 
+const numeroCaracteres = document.querySelector('#cantidad');
+let botonGenerar = document.querySelector('#generar');
+botonGenerar.addEventListener('click', generarPassword);
+const cantidad = parseInt(numeroCaracteres.value);
+
+function generarPassword(){
+    const cadenaCaracteres = "abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUV0123456789";
+    console.log(cantidad)
+    if(cantidad > 0){
+        for(let i=0; i<cantidad;i++){
+            console.log(i);
+        }
+    }
+}
+
+function aleatorioCaracter(cadena){
+    const endString = cadena.length;
+    return Math.floor(Math.random() * endString);
+}
+
+console.log(aleatorioCaracter("aassd"))
 
 
 
