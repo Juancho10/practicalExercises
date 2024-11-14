f978a40344812675fe81da6f91502c74b1286fdf
Juancho10 <juanitolizarazu18@gmail.com>
2024-11-14 15:13:46 -0500

    ExerciseOneHalloweenDev

--- JavaScript/exercise_two/app.js
+++ JavaScript/exercise_two/app.js
@@ -1,7 +1,24 @@
-let numeroInicial = 0;
+//Halloween Dev Juego uno - Poción perfecta
+//potions => []
+export function createMagicPotion (potions, target){
+    const seen = {};
 
-for (let i = 1; i <= 100; i++) {
-    numeroInicial += i;
+    for (let i = 0; i < potions.length; i++) {
+        //simplemente se almacena la posición que se esta calculando
+        const currentPotion = potions[i];
+        console.log(currentPotion);
+        // se toma el complemento o la parte que le hace falta a cada posión ejemplo de 1 el complemento para se 5 es 4, se calcula la diferencia
+        const complement = target - potions[i];
+        console.log(complement);
+        
+        //calcular que el complemento se encuentre en el array de posiones
+        seen[complement];
+        console.log(seen);
+    }
     
 }
-console.log(numeroInicial);
+
+const potions = [1, 2, 3];
+const target = 5;
+
+createMagicPotion(potions, target);
