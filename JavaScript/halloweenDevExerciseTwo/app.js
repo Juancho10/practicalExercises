a6c51f0e4f93aeb3f8218b5c10dd9d2aa3e0e4a6
Juancho10 <juanitolizarazu18@gmail.com>
2024-12-13 16:56:19 -0500

    add first steps second exeercises

--- JavaScript/halloweenDevExerciseTwo/app.js
+++ JavaScript/halloweenDevExerciseTwo/app.js
@@ -1,22 +1,12 @@
-
-function multiplicar(numero,maximo){
-    
-    for(i=0;i<=maximo;i++){
-        let resultado = numero * i;
-        console.log(numero+" * "+i+" = "+ resultado);
-    }
-    
+function battleHorde(zombies, humans) {
+    // Code here
+    return 'x'
 }
 
-console.log(multiplicar(4,50));
-/*let numero = 2;
-for (i=0; i<11; i++){
-    let resultado1 = numero * i;
-    console.log(numero+" * "+i+" = "+ resultado1);
-    //console.log("el numero i es: "+ i);
-    for(j=0; j<11; j++){
-        //console.log("el numero j es: "+j);
-        let resultado = i * j;
-        console.log(i+" * "+j+" = "+ resultado);
-    }
-}*/
+function battleExample(){
+    zombies = '123';
+    humans = '321';
+    console.log("your value is: "+zombies);
+}
+
+battleExample();
