80aede25aa42d59c6d2cd576d76a68bef91f2bc4
Juancho10 <juanitolizarazu18@gmail.com>
2024-12-20 11:19:06 -0500

    finish exercise two

--- JavaScript/halloweenDevExerciseTwo/app.js
+++ JavaScript/halloweenDevExerciseTwo/app.js
@@ -15,8 +15,7 @@ function battleExample(zombies, humans){
         console.log(`la el valor del zombie es: ${currentPotitionZombie} y el del humano es ${currentPotitionHuman}`);
         zombie += currentPotitionZombie;
         human += currentPotitionHuman;
-
-
+        console.log(Math.abs(zombie-human));
         /*
         if(human>zombie[i]){
             human += humans[i]-zombies[i]
@@ -26,14 +25,19 @@ function battleExample(zombies, humans){
             console.log(result.toString+"z");
         }*/
     }
-    console.log(zombie)
-    console.log(human);
-    
-    
+    const diff = Math.abs(zombie - human);//se halla la diferencia, si es negativa se obtiene el positivo con el valor absoluto   
+    if (zombie === human) return 'x';//se evalua cuando los dos tienen el mismo poder
+    const letter = zombie > human ? 'z' : 'h';//se evalua la letra de quien gana
+    //console.log(letter)
+
+    return `el ganador es ${diff}${letter}`; 
+    //console.log(zombie)
+    //console.log(human);
 }
 const a = "242";
 const b = "334";
 
 battleExample(a,b);
+console.log(battleExample(a,b));
 
 //https://www.tldraw.com/s/v2_c_1UD9-eFRe5F5ZRhzQQOQ1?d=v0.0.1920.890.page
