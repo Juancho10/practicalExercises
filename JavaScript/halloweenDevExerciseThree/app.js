46fa4f1a5bba717300669a5702e404563a27b72e
Juancho10 <juanitolizarazu18@gmail.com>
2024-12-20 16:41:16 -0500

    add link tldraw

--- JavaScript/halloweenDevExerciseThree/app.js
+++ JavaScript/halloweenDevExerciseThree/app.js
@@ -13,10 +13,13 @@ console.log(dream.length);
 for (let row = 0; row < (dream.length); row++) {
     for (let column = 0; column < dream.length; column++) {
         console.log(`fila: ${row} y columna: ${column}`);
-        console.log(dream[row][column]);
+        const prueba = dream[row][column] === 1 ? 'true': 'false';
+        console.log(`${prueba} = ${dream[row][column]}`);
+        
     }
 }
 
+//https://www.tldraw.com/r/BJmSf1IdF0KAsfQGsmf5H?d=v-920.154.1920.890.OvYjR8o90IwF1XIKULCYS
 /*
 function findSafestPath(dream) {
 
